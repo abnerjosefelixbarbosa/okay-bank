4e58af98964386316568434393b112e4efc4cca9
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-06-05 18:43:05 -0300

    commited by Abner

--- backend-spring-boot/src/main/java/com/org/backend/BackendApplication.java
+++ backend-spring-boot/src/main/java/com/org/backend/BackendApplication.java
@@ -1,13 +1,11 @@
-package com.org.backendspringboot;
+package com.org.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringBootApplication {
-
+public class BackendApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BackendSpringBootApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
