732868aec254d6ee1a79f233ac99b0d8bd1acf7c
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-06-05 22:34:41 -0300

    commited by Abner

---
+++ backend-spring-boot/src/main/java/com/org/backend/exceptions/HandleError.java
@@ -0,0 +1,16 @@
+package com.org.backend.exceptions;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class HandleError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+}
