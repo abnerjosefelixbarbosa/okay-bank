83043fbaa4988b585bd4d083a2df6c732c37eaa1
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-06-06 18:35:50 -0300

    commited by Abner

--- backend-spring-boot/src/main/java/com/org/backend/services/CustomerService.java
+++ backend-spring-boot/src/main/java/com/org/backend/services/CustomerService.java
@@ -8,6 +8,8 @@ import com.org.backend.interfaces.CustomerMethods;
 import com.org.backend.models.Customer;
 import com.org.backend.repositories.CustomerRepository;
 
+import br.com.caelum.stella.validation.CPFValidator;
+
 @Service
 public class CustomerService implements CustomerMethods {
 	@Autowired
@@ -15,13 +17,27 @@ public class CustomerService implements CustomerMethods {
 	
 	public String save(Customer customer) {
 		validSave(customer);
-		customerRepository.save(customer);
+		//customerRepository.save(customer);
 		return "customer saved";
 	}
 	
 	private void validSave(Customer customer) {
-		if (customer.getName().isEmpty()) {
-			throw new EntityBadRequestException("Name is empty");
+		if (customer.getName().isEmpty()) { 
+			throw new EntityBadRequestException("Name empty");
+		}
+		if (!validCpf(customer.getCpf())) {
+			throw new EntityBadRequestException("CPF invalid");
+		}
+	}
+	
+	private boolean validCpf(String cpf) {
+		try {
+			CPFValidator cpfValidator = new CPFValidator();
+			String newCpf = cpf.replace(".", "").replace("-", "");
+			cpfValidator.assertValid(newCpf);
+			return true;
+		} catch (Exception e) {
+			return false;
 		}
 	}
 }
