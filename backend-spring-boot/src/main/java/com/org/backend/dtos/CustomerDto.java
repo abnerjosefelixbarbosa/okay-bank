334eecb86fb52f92d7b7da77129de46a434ab4da
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-06-09 17:34:23 -0300

    commited by Abner

--- backend-spring-boot/src/main/java/com/org/backend/dtos/CustomerDto.java
+++ backend-spring-boot/src/main/java/com/org/backend/dtos/CustomerDto.java
@@ -3,6 +3,10 @@ package com.org.backend.dtos;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import com.org.backend.exceptions.EntityBadRequestException;
+
+import br.com.caelum.stella.validation.CPFValidator;
+import jakarta.mail.internet.InternetAddress;
 import lombok.Data;
 
 @Data
@@ -21,4 +25,84 @@ public class CustomerDto {
 	private String addressNeighborhood;
 	private String addressCity;
 	private String addressState;
+	
+	public void validation() {
+		if (name == null || name.isEmpty()) 
+			throw new EntityBadRequestException("name is empty");
+		if (name.length() > 100) 
+			throw new EntityBadRequestException("name is greater than 100 characters");
+		if (!validCpf(cpf)) 
+			throw new EntityBadRequestException("cpf is invalid");
+		if (rg == null || rg.isEmpty()) 
+			throw new EntityBadRequestException("rg is empty");
+		if (rg.contains(" ")) 
+			throw new EntityBadRequestException("rg contains white space");
+		if (rg.length() > 20) 
+			throw new EntityBadRequestException("rg is greater than 20 characters");
+		if (password == null || password.length() != 6) 
+			throw new EntityBadRequestException("password is diferent for 6 characters");
+		if (password.contains(" ")) 
+			throw new EntityBadRequestException("password contains white");
+		if (!validEmail(email)) 
+			throw new EntityBadRequestException("email is invalid");
+		if (email.length() > 50) 
+			throw new EntityBadRequestException("email is greater than 50 characters");
+		if (telephone == null || telephone.isEmpty()) 
+			throw new EntityBadRequestException("telephone is empty");
+		if (telephone.length() > 20) 
+			throw new EntityBadRequestException("telephone is greater than 20 characters");
+		if (birthDate == null) 
+			throw new EntityBadRequestException("birth date is null");
+		var localDate = LocalDate.now();
+		var yearCalculated = localDate.getYear() - birthDate.getYear();
+		if (birthDate.isAfter(localDate) || yearCalculated < 18 || birthDate.getYear() < 1950) 
+			throw new EntityBadRequestException("birth date invalid");
+		if (addressNumber == null || addressNumber == 0) 
+			throw new EntityBadRequestException("address number is 0");
+		if (addressZipCode == null || addressZipCode.isEmpty()) 
+			throw new EntityBadRequestException("address zip code is empty");
+		if (addressZipCode.contains(" ")) 
+			throw new EntityBadRequestException("address zip code contains white space");
+		if (addressZipCode.length() > 20) 
+			throw new EntityBadRequestException("address zip code is greater than 20 characters");
+		if (addressName == null || addressName.isEmpty()) 
+			throw new EntityBadRequestException("address name is empty");
+		if (addressName.length() > 50) 
+			throw new EntityBadRequestException("address name is greater than 50 characters");
+		if (addressNeighborhood == null || addressNeighborhood.isEmpty()) 
+			throw new EntityBadRequestException("address neighborhood is empty");
+		if (addressNeighborhood.length() > 30) 
+			throw new EntityBadRequestException("address neighborhood is greater than 30 characters");
+		if (addressCity == null || addressCity.isEmpty()) 
+			throw new EntityBadRequestException("address city is empty");
+		if (addressCity.length() > 30) 
+			throw new EntityBadRequestException("address city is greater than 30 characters");
+		if (addressState == null || addressState.isEmpty()) 
+			throw new EntityBadRequestException("address state is empty");
+		if (addressState.contains(" ")) 
+			throw new EntityBadRequestException("address state contains white space");
+		if (addressState.length() != 2) 
+			throw new EntityBadRequestException("address state is diferent for 2 characters");
+	}
+	
+	private boolean validCpf(String cpf) {
+		try {
+			CPFValidator cpfValidator = new CPFValidator();
+			String newCpf = cpf.replace(".", "").replace("-", "");
+			cpfValidator.assertValid(newCpf);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private boolean validEmail(String email) {
+		try {
+			InternetAddress internetAddress = new InternetAddress(email);
+			internetAddress.validate();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
--- backend-spring-boot/src/main/java/com/org/backend/services/CustomerService.java
+++ backend-spring-boot/src/main/java/com/org/backend/services/CustomerService.java
@@ -1,15 +1,15 @@
 package com.org.backend.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.org.backend.exceptions.EntityBadRequestException;
+import com.org.backend.exceptions.EntityNotFoundException;
 import com.org.backend.interfaces.CustomerMethods;
 import com.org.backend.models.CustomerModel;
 import com.org.backend.repositories.CustomerRepository;
-
-import br.com.caelum.stella.validation.CPFValidator;
-import jakarta.mail.internet.InternetAddress;
 
 @Service
 public class CustomerService implements CustomerMethods {
@@ -17,84 +17,36 @@ public class CustomerService implements CustomerMethods {
 	private CustomerRepository customerRepository;
 
 	public String save(CustomerModel customerModel) {
-		validSave(customerModel);
-		// customerRepository.save(customerModel);
+		validSaveUpdate(customerModel);
+		customerModel.setId(null);
+		customerRepository.save(customerModel);
 		return "customer saved";
 	}
+	
+	public String update(UUID id, CustomerModel customerModel) {
+		CustomerModel findById = findById(id);
+		customerModel.setId(findById.getId());
+		validSaveUpdate(customerModel);
+		customerRepository.save(customerModel);
+		return "customer updated";
+	}
+	
+	private CustomerModel findById(UUID id) {
+		return customerRepository.findById(id).orElseThrow(() -> {
+			return new EntityNotFoundException("id not found");
+		});
+	}
 
-	private void validSave(CustomerModel customerModel) {
-		if (customerModel.getName().isEmpty() || customerModel.getName() == null) 
-			throw new EntityBadRequestException("name is empty");
-		if (customerModel.getName().length() > 100) 
-			throw new EntityBadRequestException("name is greater than 100");
-		if (!validCpf(customerModel.getCpf())) 
-			throw new EntityBadRequestException("cpf is invalid");
-		if (customerModel.getRg().isBlank() || customerModel.getRg() == null) 
-			throw new EntityBadRequestException("rg is blank");
-		if (customerModel.getRg().length() > 20) 
-			throw new EntityBadRequestException("rg is greater than 20");
-		if (customerModel.getPassword().length() != 6) 
-			throw new EntityBadRequestException("password is diferent for 6");
-		if (!validEmail(customerModel.getEmail())) 
-			throw new EntityBadRequestException("email is invalid");
-		if (customerModel.getTelephone().isEmpty() || customerModel.getTelephone() == null) 
-			throw new EntityBadRequestException("telephone is empty");
-		if (customerModel.getTelephone().length() > 20) 
-			throw new EntityBadRequestException("telephone is greater than 20");
-		if (customerModel.getBirthDate() == null) 
-			throw new EntityBadRequestException("birth date is null");
-		if (customerModel.getAddressNumber() == 0 || customerModel.getAddressNumber() == null) 
-			throw new EntityBadRequestException("address number is 0");
-		if (customerModel.getAddressZipCode().isBlank() || customerModel.getAddressZipCode() == null) 
-			throw new EntityBadRequestException("address zip code is blank");
-		if (customerModel.getAddressZipCode().length() > 20) 
-			throw new EntityBadRequestException("address zip code is greater than 20");
-		if (customerModel.getAddressName().isEmpty() || customerModel.getAddressName() == null) 
-			throw new EntityBadRequestException("address name is empty");
-		if (customerModel.getAddressName().length() > 50) 
-			throw new EntityBadRequestException("address name is greater than 50");
-		if (customerModel.getAddressNeighborhood().isEmpty() || customerModel.getAddressNeighborhood() == null) 
-			throw new EntityBadRequestException("address neighborhood is empty");
-		if (customerModel.getAddressNeighborhood().length() > 30) 
-			throw new EntityBadRequestException("address neighborhood is greater than 30");
-		if (customerModel.getAddressCity().isEmpty() || customerModel.getAddressCity() == null) 
-			throw new EntityBadRequestException("address city is empty");
-		if (customerModel.getAddressCity().length() > 30) 
-			throw new EntityBadRequestException("address city is greater than 30");
-		if (customerModel.getAddressState().isBlank() || customerModel.getAddressState() == null) 
-			throw new EntityBadRequestException("address state is blank");
-		if (customerModel.getAddressState().length() > 2) 
-			throw new EntityBadRequestException("address state is greater than 2");
+	private void validSaveUpdate(CustomerModel customerModel) {
 		if (customerRepository.existsByCpf(customerModel.getCpf())) 
-			throw new EntityBadRequestException("Cpf exists");
+			throw new EntityBadRequestException("cpf exists");
 		if (customerRepository.existsByRg(customerModel.getRg())) 
-			throw new EntityBadRequestException("Rg exists");
+			throw new EntityBadRequestException("rg exists");
 		if (customerRepository.existsByPassword(customerModel.getPassword())) 
-			throw new EntityBadRequestException("Password exists");
+			throw new EntityBadRequestException("password exists");
 		if (customerRepository.existsByEmail(customerModel.getEmail())) 
-			throw new EntityBadRequestException("Email exists");
+			throw new EntityBadRequestException("email exists");
 		if (customerRepository.existsByTelephone(customerModel.getTelephone())) 
-			throw new EntityBadRequestException("Telephone exists");
-	}
-
-	private boolean validCpf(String cpf) {
-		try {
-			CPFValidator cpfValidator = new CPFValidator();
-			String newCpf = cpf.replace(".", "").replace("-", "");
-			cpfValidator.assertValid(newCpf);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
-
-	private boolean validEmail(String email) {
-		try {
-			InternetAddress internetAddress = new InternetAddress(email);
-			internetAddress.validate();
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
+			throw new EntityBadRequestException("telephone exists");
 	}
 }
