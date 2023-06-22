9a74cc208e32d7894a18bc12c0541bb309f0e9df
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2023-06-22 18:51:35 -0300

    fix project

--- frontend-react/src/pages/Login/Login.tsx
+++ frontend-react/src/pages/Login/Login.tsx
@@ -1,22 +1,13 @@
-import Position from "../../models/Position";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Login() {
-  
 
-  function test(): void {
-    const newPosition = new Position('asas', '', 0);
-    console.log(newPosition.getPosition());
-  }
 
   return (
     <>
       <header></header>
       <section>
         <h1>Login</h1>
-        <button type="button" onClick={test}>
-          Test
-        </button>
       </section>
       <footer></footer>
     </>
