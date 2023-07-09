8b288f7e664637f2a4ca4f1938eaefdefe65710f
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2023-07-09 16:17:05 -0300

    committed by Abner

--- frontend-react/src/pages/ConfirmeBalance/ConfirmeBalance.tsx
+++ frontend-react/src/pages/ConfirmeBalance/ConfirmeBalance.tsx
@@ -1,5 +1,46 @@
 import "./style.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export function ConfirmeBalance() {
-  return <></>;
+  return (
+    <>
+      <FormConfirmeBalance />
+    </>
+  );
+}
+
+function FormConfirmeBalance() {
+
+
+  function onConfirm() {
+    alert("test");
+  }
+
+  return (
+    <>
+      <div className="ajust">
+        <Container className="container_balance_form">
+          <Row>
+            <Form onSubmit={(e) => {
+              e.preventDefault();
+              onConfirm();
+            }}>
+              <Form.Group className="mb-3">
+                <Form.Label>Balance</Form.Label>
+                <Form.Control type="number" />
+              </Form.Group>
+              <div className="d-grid gap-2">
+                <Button variant="primary" type="submit" size="lg">
+                  Confirm
+                </Button>
+              </div>
+            </Form>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
