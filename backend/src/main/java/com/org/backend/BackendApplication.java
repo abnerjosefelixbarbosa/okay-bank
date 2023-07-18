9f00281c82403ae3f3b9e5959c72e3f894fa964a
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2023-07-18 16:03:43 -0300

    committed by Abner

--- backend/src/main/java/com/org/backend/BackendApplication.java
+++ backend/src/main/java/com/org/backend/BackendApplication.java
@@ -1,11 +1,21 @@
 package com.org.backend;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+	@Value("${info}")
+    private String text;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(text);
 	}
 }
