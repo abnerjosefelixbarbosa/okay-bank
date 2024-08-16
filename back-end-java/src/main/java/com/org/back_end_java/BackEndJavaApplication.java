6a8ef19ab6945626448fa9820b5031d1f8becab2
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-08-16 19:46:01 -0300

    Application properties configuration

--- back-end-java/src/main/java/com/org/back_end_java/BackEndJavaApplication.java
+++ back-end-java/src/main/java/com/org/back_end_java/BackEndJavaApplication.java
@@ -1,13 +1,20 @@
 package com.org.back_end_java;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndJavaApplication {
+public class BackEndJavaApplication implements CommandLineRunner {
+	@Value("${info}")
+	private String info;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndJavaApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		System.out.println(info);
+	}
 }
