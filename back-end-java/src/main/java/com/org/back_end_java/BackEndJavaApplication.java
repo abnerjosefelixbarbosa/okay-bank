7ecd5b0196250401c273de865fba319d6729a54c
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-08-16 18:56:50 -0300

    Initial commit

---
+++ back-end-java/src/main/java/com/org/back_end_java/BackEndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.org.back_end_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndJavaApplication.class, args);
+	}
+
+}
---
+++ back-end-java/src/test/java/com/org/back_end_java/BackEndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.back_end_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
