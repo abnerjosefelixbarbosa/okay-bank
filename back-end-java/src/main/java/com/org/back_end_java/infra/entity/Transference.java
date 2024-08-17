c4c432b30a3b2bdcf5672c3889222052bf5099fd
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-08-17 00:32:10 -0300

    create entity db

--- back-end-java/src/main/java/com/org/back_end_java/infra/entity/Transference.java
+++ back-end-java/src/main/java/com/org/back_end_java/infra/entity/Transference.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,4 +27,10 @@ public class Transference implements Serializable {
 	private LocalDateTime dateTime;
 	@Column(nullable = false)
 	private BigDecimal valueTransference;
+	@ManyToOne
+	@JoinColumn(name = "id_customer", nullable = false)
+	private Customer customer;
+	@ManyToOne
+	@JoinColumn(name = "id_recipient", nullable = false)
+	private Recipient recipient;
 }
