c608519b97549fe57a5c67599c3366009f422b99
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-09-02 23:00:00 -0300

    commited by Abner

--- backend-java/src/test/java/com/api/backend_java/controller/AgencyControllerTest.java
+++ backend-java/src/test/java/com/api/backend_java/controller/AgencyControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import com.api.backend_java.domain.dto.AgencyDTO;
 import com.api.backend_java.infra.entity.Agency;
