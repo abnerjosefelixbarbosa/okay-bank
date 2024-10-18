ba11951577cae076ef75a9f402215757ba702798
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-10-18 18:53:13 -0300

    commited by Abner

--- frontend-angula/src/app/pages/login/login.component.ts
+++ frontend-angula/src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
